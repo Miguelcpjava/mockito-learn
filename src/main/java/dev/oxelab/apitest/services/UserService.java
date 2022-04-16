56c4afabd65a13ba54bd0979c0b92ca9b868ae25
miguellima <miguelcpjava@gmail.com>
2022-04-16 10:29:32 -0300

    Criando e refatorando o endpoint para acesso a lista de usuarios.

--- src/main/java/dev/oxelab/apitest/services/UserService.java
+++ src/main/java/dev/oxelab/apitest/services/UserService.java
@@ -2,7 +2,10 @@ package dev.oxelab.apitest.services;
 
 import dev.oxelab.apitest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/dev/oxelab/apitest/services/impl/UserServiceImpl.java
+++ src/main/java/dev/oxelab/apitest/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import dev.oxelab.apitest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +22,7 @@ public class UserServiceImpl implements UserService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 
+    public List<User> findAll(){
+        return repository.findAll();
+    }
 }
