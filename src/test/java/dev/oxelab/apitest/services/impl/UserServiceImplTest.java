3da9abc268590da6feaaf39634cf9d931e7c281c
miguellima <miguelcpjava@gmail.com>
2022-04-19 09:18:30 -0300

    Adicionando teste para a classe userservice para o metodo delete para o retorno com excessao.

--- src/test/java/dev/oxelab/apitest/services/impl/UserServiceImplTest.java
+++ src/test/java/dev/oxelab/apitest/services/impl/UserServiceImplTest.java
@@ -168,6 +168,19 @@ class UserServiceImplTest {
 
     }
 
+    @Test
+    void deleteWithObjectNotFoundException(){
+        when(repository.findById(anyInt()))
+                .thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+        try{
+            service.delete(ID);
+        }catch (Exception exception){
+            assertEquals(ObjectNotFoundException.class,exception.getClass());
+            assertEquals(OBJETO_NAO_ENCONTRADO,exception.getMessage());
+        }
+    }
+
+
     private void startUser(){
         user = new User(ID, NAME, EMAIL, PASSWORD);
         userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
