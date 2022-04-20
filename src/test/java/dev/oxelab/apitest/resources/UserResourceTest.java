beda41c543e8a7188322331c018ff8aa25f98d06
miguellima <miguelcpjava@gmail.com>
2022-04-19 22:36:47 -0300

    Adicionando teste para a classe UserResource.

---
+++ src/test/java/dev/oxelab/apitest/resources/UserResourceTest.java
@@ -0,0 +1,68 @@
+package dev.oxelab.apitest.resources;
+
+import dev.oxelab.apitest.domain.User;
+import dev.oxelab.apitest.domain.dto.UserDTO;
+import dev.oxelab.apitest.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final int ID          = 1;
+    public static final String NAME     = "Miguel";
+    public static final String EMAIL    = "miguel@mail.com";
+    public static final String PASSWORD = "123";
+
+
+    @InjectMocks
+    private UserResource resource;
+    @Mock
+    private UserServiceImpl service;
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
